5219f03a2df2e15c64702d8deafb32a832e05562
shaoneng <wushaoneng@qq.com>
2025-06-15 21:52:58 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-<<<<<<< HEAD
 
 import tailwindcss from '@tailwindcss/vite';
 
@@ -9,15 +8,5 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   }
-=======
-import tailwind from "@astrojs/tailwind"; // 由第一步命令自动添加
-import image from "@astrojs/image";     // 由第二步命令自动添加
 
-// https://astro.build/config
-export default defineConfig({
-  integrations: [
-    tailwind(), // 自动添加
-    image()     // 自动添加
-  ]
->>>>>>> 96d7c3ebcdae202ba01a55fbb22e4123072531b6
 });
